19288efd1c2120dc016669e152d140aaa71ab935
Andrew Holt <ajh.206@outlook.com>
2019-01-14 21:23:39 -0800

    Update GA_Kaggle.py

--- GA_Kaggle.py
+++ GA_Kaggle.py
@@ -17,9 +17,14 @@ print(df.describe())
 target = df["Chance of Admit "].values
 features = df.drop(["Chance of Admit ", 'Serial No.'],axis=1)
 
-# Viz
-features.hist()
-plt.show()
+# Viz - Plot each feature as histogram 
+for each in features:
+    features[each].hist()
+    plt.title(each+ ' distribution')
+    plt.xlabel(each)
+    plt.ylabel("Observations\n(Count)")
+    plt.show()
+
 
 # test, train
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size = 0.30, random_state = 1)
